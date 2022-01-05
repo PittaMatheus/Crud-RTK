dd5ceb66154aa9a46f848e0db5c13b7ba3e48aaf
Matheus <matheus.pitta12@gmail.com>
2022-01-05 00:53:52 -0300

    RTK struct example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom'
+import { PostsManager } from './features/posts/PostsManager'
+import { Box } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box>
+      <Switch>
+        <Route path="/" component={PostsManager} />
+      </Switch>
+    </Box>
+  )
 }
 
-export default App;
+export default App
